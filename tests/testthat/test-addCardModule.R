d81155ceeb7d1136160ed9d1febb51345c2f2379
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-04-21 11:50:16 +0200

    add first testServer

---
+++ tests/testthat/test-addCardModule.R
@@ -0,0 +1,33 @@
+card1 <- ReportCard$new()
+card1$append_text("Header 2 text", "header2")
+card1$append_text("A paragraph of default text", "header2")
+card1$append_plot(
+  ggplot2::ggplot(iris, ggplot2::aes(x = Petal.Length)) +
+    ggplot2::geom_histogram()
+)
+
+reporter <- Reporter$new()
+
+testthat::test_that("Adding comment header and comment body", {
+  shiny::testServer(add_card_button_srv,
+                    args = list(reporter = reporter, card = reactive(card1)), {
+    card_len <- length(card()$get_content())
+    session$setInputs(comment = "Comment Body")
+    session$setInputs(`addCardOk` = 0)
+
+    testthat::expect_identical(
+      length(reporter$get_blocks()),
+      card_len + 2L
+    )
+
+    testthat::expect_identical(
+        tail(reporter$get_blocks(), 1)[[1]]$get_content(),
+        "Comment Body"
+    )
+
+    testthat::expect_identical(
+      tail(reporter$get_blocks(), 2)[[1]]$get_content(),
+      "Comment"
+    )
+  })
+})
