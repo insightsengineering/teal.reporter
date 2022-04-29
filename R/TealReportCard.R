a16b7a85900a2be268019c51c7a48d79f003848f
Mahmoud Hallal <86970066+mhallal1@users.noreply.github.com>
2022-04-29 09:13:57 +0200

    tealReportCard class + meta_data (#24)
    * TealReportCard
    
    * meta_data
    
    * update TealReportCard
    
    * append instead of insert
    
    * tests1
    
    * tests2
    
    * update docs
    
    * fix merge conflict
    
    * Empty commit
    
    * docs + tests
    
    * remove ListBlock
    
    * lintr
    
    * fix example
    
    * review 1
    
    * update tests
    
    * review 2
    
    * docs
    
    * docs
    
    * review 3
    
    * deparse (#25)
    
    * deparse
    
    * green
    
    * review
    
    * tests
    
    * fix
    
    * green
    
    * docs
    
    * review Maciej
    
    * Konrad review
    
    * lintr
    
    * replace deparse with chr_converter
    
    Co-authored-by: arkadiuszbeer <86738093+arkadiuszbeer@users.noreply.github.com>
    Co-authored-by: Maciej Nasinski <nasinski.maciej@gmail.com>

---
+++ R/TealReportCard.R
@@ -0,0 +1,60 @@
+#' @title `TealReportCard`
+#' @description A child of `ReportCard` that is used for teal specific applications.
+#' In addition to the parent methods, it supports rendering teal specific elements such as
+#' the source code, the encodings panel content and the filter panel content as part of the
+#' meta data.
+#' @export
+#'
+TealReportCard <- R6::R6Class( # nolint: object_name_linter.
+  classname = "TealReportCard",
+  inherit = ReportCard,
+  public = list(
+    #' @description Appends the source code to the `content` meta data of this `TealReportCard`.
+    #'
+    #' @param src (`character(1)`) code as text.
+    #' @param chr_converter (`function`) to convert `src` argument to a string,
+    #' by default `function(x) paste0("```\n", paste(x, collapse = "\n"), "\n```\n")`.
+    #' @return invisibly self
+    #' @examples
+    #' card <- TealReportCard$new()$append_src(
+    #'   "ggplot2::ggplot(iris, ggplot2::aes(x = Petal.Length)) + ggplot2::geom_histogram()"
+    #' )
+    #' card$get_content()[[1]]$get_content()
+    append_src = function(src, chr_converter = function(x) paste0("```\n", paste(x, collapse = "\n"), "\n```\n")) {
+      checkmate::assert_character(src, min.len = 0, max.len = 1)
+      super$append_metadata("SRC", src, chr_converter)
+      invisible(self)
+    },
+    #' @description Appends the filter state list to the `content` meta data  of this `TealReportCard`.
+    #'
+    #' @param fs (`list`) list of filter states.
+    #' @param chr_converter (`function`) to convert `fs` argument to a string, by default `base::deparse1`.
+    #' @return invisibly self
+    #' @examples
+    #' card <- TealReportCard$new()$append_fs(
+    #'   list(data = list(X = list(selected = c(1, 10))))
+    #' )
+    #' card$get_content()[[1]]$get_content()
+    #'
+    append_fs = function(fs, chr_converter = deparse1) {
+      checkmate::assert_list(fs)
+      super$append_metadata("FS", fs, chr_converter)
+      invisible(self)
+    },
+    #' @description Appends the encodings list to the `content` meta data of this `TealReportCard`.
+    #'
+    #' @param encodings (`list`) list of encodings selections of the teal app.
+    #' @param chr_converter (`function`) to convert a encodings to a string, by default `base::deparse1`.
+    #' @return invisibly self
+    #' @examples
+    #' card <- TealReportCard$new()$append_encodings(list("variable 1 is X"))
+    #' card$get_content()[[1]]$get_content()
+    #'
+    append_encodings = function(encodings, chr_converter = deparse1) {
+      checkmate::assert_list(encodings)
+      super$append_metadata("Encodings", encodings, chr_converter)
+      invisible(self)
+    }
+  ),
+  private = list()
+)
---
+++ tests/testthat/test-TealReportCard.R
@@ -0,0 +1,57 @@
+testthat::test_that("TealReportCard object can be initialized", {
+  testthat::expect_error(TealReportCard$new(), regexp = NA)
+})
+
+testthat::test_that("TealReportCard inherits from ReportCard", {
+  testthat::expect_true(inherits(TealReportCard$new(), "ReportCard"))
+})
+
+testthat::test_that("TealReportCard$new returns an object of type TealReportCard", {
+  testthat::expect_true(inherits(TealReportCard$new(), "TealReportCard"))
+})
+
+testthat::test_that("TealReportCard$get_content returns a list of ContentBlock objects", {
+  card <- TealReportCard$new()$append_text("test")$append_plot(ggplot2::ggplot(iris))$append_table(iris)
+  testthat::expect_true(checkmate::test_list(card$get_content(), types = "ContentBlock"))
+})
+
+testthat::test_that("TealReportCard$get_content returns content with metadata", {
+  card <- TealReportCard$new()$append_text("test")$append_src("test_src")$append_encodings(list("data = test"))
+  testthat::expect_equal(length(card$get_content()), 3)
+  testthat::expect_identical(card$get_content()[[1]]$get_content(), "test")
+  testthat::expect_identical(
+    card$get_content()[[2]]$get_content(),
+    paste0("```\n", paste("test_src", collapse = "\n"), "\n```\n")
+  )
+  testthat::expect_identical(card$get_content()[[3]]$get_content(), deparse1(list("data = test")))
+})
+
+testthat::test_that("TealReportCard$append_src accepts a character", {
+  card <- TealReportCard$new()
+  testthat::expect_error(card$append_src("test"), regexp = NA)
+})
+
+testthat::test_that("TealReportCard$append_src returns self", {
+  card <- TealReportCard$new()
+  testthat::expect_identical(card$append_src("test"), card)
+})
+
+testthat::test_that("TealReportCard$append_encodings accepts list of character", {
+  card <- TealReportCard$new()
+  testthat::expect_error(card$append_encodings(list("test")), regexp = NA)
+})
+
+testthat::test_that("TealReportCard$append_encodings returns self", {
+  card <- TealReportCard$new()
+  testthat::expect_identical(card$append_encodings(list("test_encodings")), card)
+})
+
+testthat::test_that("TealReportCard$append_fs accepts a list of character", {
+  card <- TealReportCard$new()
+  testthat::expect_error(card$append_fs(list("test")), regexp = NA)
+})
+
+testthat::test_that("TealReportCard$append_fs returns self", {
+  card <- TealReportCard$new()
+  testthat::expect_identical(card$append_fs(list("test_fs")), card)
+})
