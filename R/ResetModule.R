c7822ab020e3f8b5231b130587154ae34ec67eeb
Mahmoud Hallal <86970066+mhallal1@users.noreply.github.com>
2022-05-09 11:53:40 +0200

    Simple reporter updates (#32)
    * resetModule
    
    * error notification
    
    * no comment
    
    * spelling
    
    * add card notification
    
    * * changed the inputText comment area in add_card_button_srv to
      textAreaInput
    * replaced the default value of the said input with a placeholder
      value and changed its wording
    * changed the colors of cancel button in SimpleReporter to red
    
    * Update R/ResetModule.R
    
    Co-authored-by: Konrad Pagacz <konrad.pagacz@gmail.com>
    
    * update comment check
    
    Co-authored-by: kpagacz <konrad.pagacz@gmail.com>

---
+++ R/ResetModule.R
@@ -0,0 +1,69 @@
+#' Reset Button Reporter User Interface
+#' @description button for resetting the report content.
+#'
+#' For more details see the vignette: `vignette("simpleReporter", "teal.reporter")`.
+#' @param id `character`
+#' @return `shiny::tagList`
+#' @export
+reset_report_button_ui <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::tags$button(
+      id = ns("reset_reporter"),
+      type = "button",
+      class = "btn btn-warning action-button",
+      `data-val` = shiny::restoreInput(id = ns("reset_reporter"), default = NULL),
+      NULL,
+      "Reset Reporter"
+    )
+  )
+}
+
+#' Reset Button Server
+#' @description server for resetting the Report content.
+#'
+#' For more details see the vignette: `vignette("simpleReporter", "teal.reporter")`.
+#' @param id `character`
+#' @param reporter `Reporter` instance.
+#' @return `shiny::moduleServer`
+#' @export
+reset_report_button_srv <- function(id, reporter) {
+  checkmate::assert_class(reporter, "Reporter")
+
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+      ns <- session$ns
+      nr_cards <- length(reporter$get_cards())
+
+
+      shiny::observeEvent(input$reset_reporter, {
+        shiny::showModal(
+          shiny::modalDialog(
+            shiny::tags$h3("Reset the Report"),
+            shiny::tags$hr(),
+            shiny::tags$strong(shiny::tags$p(
+              "Are you sure you want to reset the report? (This will remove ALL previously added cards)."
+            )),
+            footer = shiny::tagList(
+              shiny::tags$button(
+                type = "button",
+                class = "btn btn-danger",
+                `data-dismiss` = "modal",
+                `data-bs-dismiss` = "modal",
+                NULL,
+                "Cancel"
+              ),
+              shiny::actionButton(ns("reset_reporter_ok"), "Reset", class = "btn-warning")
+            )
+          )
+        )
+      })
+
+      shiny::observeEvent(input$reset_reporter_ok, {
+        reporter$reset()
+        shiny::removeModal()
+      })
+    }
+  )
+}
---
+++ tests/testthat/test-ResetModule.R
@@ -0,0 +1,26 @@
+card_fun <- function(card = ReportCard$new(), comment = NULL) {
+  card$append_text("Header 2 text", "header2")
+  card$append_text("A paragraph of default text", "header2")
+  card$append_plot(
+    ggplot2::ggplot(iris, ggplot2::aes(x = Petal.Length)) +
+      ggplot2::geom_histogram()
+  )
+  card
+}
+
+card1 <- card_fun()
+reporter <- Reporter$new()
+reporter$append_cards(list(card1))
+
+testthat::test_that("simple_reporter_srv - reset a reporter", {
+  shiny::testServer(
+    simple_reporter_srv,
+    args = list(reporter = reporter, notification = FALSE, card_fun = card_fun),
+    expr = {
+      testthat::expect_identical(reporter$get_cards(), list(card1))
+      session$setInputs(`reset_button_simple-reset_reporter` = 0)
+      session$setInputs(`reset_button_simple-reset_reporter_ok` = 0)
+      testthat::expect_identical(reporter$get_blocks(), list())
+    }
+  )
+})
