b6cfc9ea963b7ccc78af331a78d6ed0fc679e1bb
kpagacz <konrad.pagacz@gmail.com>
2022-05-24 11:50:56 +0200

    fix: fixed a bug where not providing the default to card would break reporting

--- R/AddCardModule.R
+++ R/AddCardModule.R
@@ -82,10 +82,11 @@ add_card_button_srv <- function(id, reporter, card_fun) {
 
       shiny::observeEvent(input$add_card_ok, {
         card_fun_args_nams <- names(formals(card_fun))
+        default_card <- formals(card_fun)[[1]]
         card <- `if`(
-          is.null(formals(card_fun)[[1]]),
+          missing(default_card),
           ReportCard$new(),
-          eval(formals(card_fun)[[1]], envir = environment(card_fun))
+          eval(defualt_card, envir = environment(card_fun))
         )
         if (length(card_fun_args_nams) == 1) {
           card <- card_fun(card)
