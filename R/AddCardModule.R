25a3e042bdebc5947a84005cea2b57acac53533b
Konrad Pagacz <konrad.pagacz@gmail.com>
2022-05-24 18:22:52 +0200

    Update R/AddCardModule.R

--- R/AddCardModule.R
+++ R/AddCardModule.R
@@ -84,8 +84,7 @@ add_card_button_srv <- function(id, reporter, card_fun) {
         card_fun_args_nams <- names(formals(card_fun))
         # The default_card is defined here because formals() returns a pairedlist object
         # of formal parameter names and their default values. The values are missing
-        # if not defined and the missing check does not work if supplied
-        # formals(card_fun)[[1]]
+        # if not defined and the missing check does not work if supplied formals(card_fun)[[1]]
         default_card <- formals(card_fun)[[1]]
         card <- `if`(
           missing(default_card),
