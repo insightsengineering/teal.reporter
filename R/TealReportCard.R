ed39f6df8c3a0eeaa749f99d59b6a7a0fbb079c6
Mahmoud Hallal <86970066+mhallal1@users.noreply.github.com>
2022-07-21 17:27:10 +0200

    update append_fs (#119)
    * update append_fs
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * review
    
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Nikolas Burkoff <nikolas.burkoff@roche.com>

--- R/TealReportCard.R
+++ R/TealReportCard.R
@@ -21,11 +21,13 @@ TealReportCard <- R6::R6Class( # nolint: object_name_linter.
     #' card$get_content()[[1]]$get_content()
     append_src = function(src) {
       checkmate::assert_character(src, min.len = 0, max.len = 1)
-      super$append_text(src, "verbatim")
-      super$append_metadata("SRC", src)
+      self$append_text(src, "verbatim")
+      self$append_metadata("SRC", src)
       invisible(self)
     },
     #' @description Appends the filter state list to the `content` and `metadata` of this `TealReportCard`.
+    #'  If the filter state list has an attribute named `formatted`, it appends it to the card otherwise it uses
+    #'  the default `yaml::as.yaml` to format the list.
     #'
     #' @param fs (`list`) of filter states.
     #' @return invisibly self
@@ -35,12 +37,19 @@ TealReportCard <- R6::R6Class( # nolint: object_name_linter.
     #'
     append_fs = function(fs) {
       checkmate::assert_list(fs)
-      super$append_text(yaml::as.yaml(fs, handlers = list(
-        POSIXct = function(x) format(x, "%Y-%m-%d"),
-        POSIXlt = function(x) format(x, "%Y-%m-%d"),
-        Date = function(x) format(x, "%Y-%m-%d")
-      )), "verbatim")
-      super$append_metadata("FS", fs)
+      attr_fs <- attr(fs, "formatted")
+      checkmate::assert_character(attr_fs, null.ok = TRUE)
+
+      if (is.null(attr_fs)) {
+        self$append_text(yaml::as.yaml(fs, handlers = list(
+          POSIXct = function(x) format(x, "%Y-%m-%d"),
+          POSIXlt = function(x) format(x, "%Y-%m-%d"),
+          Date = function(x) format(x, "%Y-%m-%d")
+        )), "verbatim")
+      } else {
+        self$append_text(attr_fs, "verbatim")
+      }
+      self$append_metadata("FS", fs)
       invisible(self)
     },
     #' @description Appends the encodings list to the `content` and `metadata` of this `TealReportCard`.
@@ -53,12 +62,12 @@ TealReportCard <- R6::R6Class( # nolint: object_name_linter.
     #'
     append_encodings = function(encodings) {
       checkmate::assert_list(encodings)
-      super$append_text(yaml::as.yaml(encodings, handlers = list(
+      self$append_text(yaml::as.yaml(encodings, handlers = list(
         POSIXct = function(x) format(x, "%Y-%m-%d"),
         POSIXlt = function(x) format(x, "%Y-%m-%d"),
         Date = function(x) format(x, "%Y-%m-%d")
       )), "verbatim")
-      super$append_metadata("Encodings", encodings)
+      self$append_metadata("Encodings", encodings)
       invisible(self)
     }
   ),
