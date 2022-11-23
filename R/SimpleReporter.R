26e3c1d516720e80c417c38094af482690dd600e
Mahmoud Hallal <mahmoud.hallal@roche.com>
2022-11-23 09:48:26 +0100

    docs

--- R/SimpleReporter.R
+++ R/SimpleReporter.R
@@ -45,7 +45,7 @@ simple_reporter_ui <- function(id) {
 #' @param id `character(1)` this `shiny` module's id.
 #' @param reporter [`Reporter`] instance.
 #' @param card_fun `function` which returns a [`ReportCard`] instance,
-#' the function has a `card` argument and optional `comment` and `label` arguments.
+#' the function has a `card` argument and an optional `comment` argument.
 #' @return `shiny::moduleServer`
 #' @export
 simple_reporter_srv <- function(id, reporter, card_fun) {
