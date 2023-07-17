9ec9e56b40a52eb616041dfb83b25e29b8b87cf6
go_gonzo <dawid.kaledkowski@gmail.com>
2023-07-17 10:40:21 +0200

    make Reporter independent on a card class when restoring from_list

--- R/Reporter.R
+++ R/Reporter.R
@@ -245,10 +245,8 @@ Reporter <- R6::R6Class( # nolint: object_name_linter.
         for (iter_c in seq_along(rlist$cards)) {
           card_class <- cards_names[iter_c]
           card <- rlist$cards[[iter_c]]
-          new_card <- switch(card_class,
-            ReportCard = ReportCard$new()$from_list(card, output_dir),
-            TealReportCard = TealReportCard$new()$from_list(card, output_dir)
-          )
+          new_card <- eval(str2lang(sprintf("%s$new()", card_class)))
+          new_card$from_list(card, output_dir)
           new_cards <- c(new_cards, new_card)
         }
       } else {
