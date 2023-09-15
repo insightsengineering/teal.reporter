4a7cfa9bcfec912c276b664440246a94edf84eba
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-09-15 11:31:50 +0530

    Update R/Renderer.R
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- R/Renderer.R
+++ R/Renderer.R
@@ -120,14 +120,17 @@ Renderer <- R6::R6Class( # nolint: object_name_linter.
       params <- block$get_params()
       params <- lapply(params, function(l) if (is.character(l)) shQuote(l) else l)
       block_content <- block$get_content()
-      paste("### ",
-        sprintf(
-          "\n```{r, %s}\n%s\n```\n",
-          paste(names(params), params, sep = "=", collapse = ", "),
-          block_content
-        ),
-        collapse = "\n"
-      )
+    paste(
+      sep = "\n",
+      collapse = "\n",
+      "### ",
+      sprintf(
+        "```{r, %s}", paste(names(params), params, sep = "=", collapse = ", ")
+      ),
+      block_content,
+      "```",
+      ""
+    )
     },
     pictureBlock2md = function(block) {
       basename_pic <- basename(block$get_content())
