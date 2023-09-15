f71c42f0e29e8632a3c5088e4edd3a4336389edb
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-09-15 17:15:33 +0530

    Update tests/testthat/test-utils.R
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,7 +6,7 @@ testthat::test_that("panel_item", {
   testthat::expect_s3_class(panel_item("LABEL", shiny::tags$div()), "shiny.tag")
 })
 
-testthat::test_that("Test to_flextable: supported class", {
+testthat::test_that("to_flextable supported class", {
   data_frame <- data.frame(A = 1:3, B = 4:6)
   flextable_output <- to_flextable(data_frame)
   testthat::expect_s3_class(flextable_output, "flextable")
