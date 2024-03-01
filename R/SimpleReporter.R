913747016c149275963c8eecf6c489233b7d47e9
Maciej Nasinski <nasinski.maciej@gmail.com>
2024-03-01 09:10:48 +0100

    custom simple reporter modules

--- R/SimpleReporter.R
+++ R/SimpleReporter.R
@@ -35,8 +35,19 @@ NULL
 
 #' @rdname simple_reporter
 #' @export
-simple_reporter_ui <- function(id) {
+simple_reporter_ui <- function(
+    id,
+    modules = getOption("teal.reporter.simple_reporter_modules", c("add", "download", "load", "reset"))
+) {
+  checkmate::assert_vector(modules, min.len = 1)
+  checkmate::assert_subset(modules, c("add", "download", "load", "reset"))
   ns <- shiny::NS(id)
+  mods <- list(
+    add = add_card_button_ui(ns("add_report_card_simple")),
+    download = download_report_button_ui(ns("download_button_simple")),
+    load = archiver_load_ui(ns("archive_load_simple")),
+    reset = reset_report_button_ui(ns("reset_button_simple"))
+  )
   shiny::tagList(
     shiny::singleton(
       shiny::tags$head(shiny::includeCSS(system.file("css/custom.css", package = "teal.reporter")))
@@ -46,10 +57,7 @@ simple_reporter_ui <- function(id) {
       shiny::tags$label(class = "text-primary block -ml-1", shiny::tags$strong("Reporter")),
       shiny::tags$div(
         class = "simple_reporter_container",
-        add_card_button_ui(ns("add_report_card_simple")),
-        download_report_button_ui(ns("download_button_simple")),
-        archiver_load_ui(ns("archive_load_simple")),
-        reset_report_button_ui(ns("reset_button_simple"))
+        mods[modules]
       )
     )
   )
@@ -57,32 +65,42 @@ simple_reporter_ui <- function(id) {
 
 #' @rdname simple_reporter
 #' @export
-simple_reporter_srv <- function(id,
-                                reporter,
-                                card_fun,
-                                global_knitr = getOption("teal.reporter.global_knitr"),
-                                rmd_output = c(
-                                  "html" = "html_document", "pdf" = "pdf_document",
-                                  "powerpoint" = "powerpoint_presentation", "word" = "word_document"
-                                ),
-                                rmd_yaml_args = list(
-                                  author = "NEST", title = "Report",
-                                  date = as.character(Sys.Date()), output = "html_document",
-                                  toc = FALSE
-                                )) {
+simple_reporter_srv <- function(
+    id,
+    reporter,
+    card_fun,
+    global_knitr = getOption("teal.reporter.global_knitr"),
+    rmd_output = c(
+      "html" = "html_document", "pdf" = "pdf_document",
+      "powerpoint" = "powerpoint_presentation", "word" = "word_document"
+    ),
+    rmd_yaml_args = list(
+      author = "NEST", title = "Report",
+      date = as.character(Sys.Date()), output = "html_document",
+      toc = FALSE
+    ),
+    modules = getOption("teal.reporter.simple_reporter_modules", c("add", "download", "load", "reset"))
+) {
+  checkmate::assert_vector(modules, min.len = 1)
+  checkmate::assert_subset(modules, c("add", "download", "load", "reset"))
+  mods <- list(
+    add = function() add_card_button_srv("add_report_card_simple", reporter = reporter, card_fun = card_fun),
+    download = function() download_report_button_srv(
+      "download_button_simple",
+      reporter = reporter,
+      global_knitr = global_knitr,
+      rmd_output = rmd_output,
+      rmd_yaml_args = rmd_yaml_args
+    ),
+    load = function() archiver_load_srv("archive_load_simple", reporter = reporter),
+    reset = function() reset_report_button_srv("reset_button_simple", reporter = reporter)
+  )
   shiny::moduleServer(
     id,
     function(input, output, session) {
-      add_card_button_srv("add_report_card_simple", reporter = reporter, card_fun = card_fun)
-      download_report_button_srv(
-        "download_button_simple",
-        reporter = reporter,
-        global_knitr = global_knitr,
-        rmd_output = rmd_output,
-        rmd_yaml_args = rmd_yaml_args
-      )
-      archiver_load_srv("archive_load_simple", reporter = reporter)
-      reset_report_button_srv("reset_button_simple", reporter = reporter)
+      for (mod in modules) {
+        mods[[mod]]()
+      }
     }
   )
 }
