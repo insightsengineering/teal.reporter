29d3aa410f7e70861ceab1e1af8a82543ccdd3ac
m7pr <marcin.kosinski.mk1@roche.com>
2025-06-06 09:11:47 +0200

    report -> card

--- R/teal_report-extract.R
+++ R/teal_report-extract.R
@@ -1,6 +1,6 @@
 #' @export
 `[.teal_report` <- function(x, names) {
   x <- NextMethod("`[`", x) # unverified doesn't need warning for code inconsistency
-  x@report <- x@report # todo: return code_chunks for given names
+  x@card <- x@card # todo: return code_chunks for given names
   x
 }
