37516bff0a1e671669b1a897b46d62bac6421faf
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-06-11 13:55:30 +0100

    fix: warning message on AppDriver creation

--- tests/testthat/helpers-previewer-shinytest2.R
+++ tests/testthat/helpers-previewer-shinytest2.R
@@ -1,11 +1,11 @@
 create_test_reporter <- function(n_cards = 2) {
   cards <- lapply(seq_len(n_cards), function(i) {
-    new_doc <- teal.reporter::teal_card(sprintf("Card %d", i))
-    teal.reporter::metadata(new_doc, "title") <- sprintf("Card %d Title", i)
+    new_doc <- teal_card(sprintf("Card %d", i))
+    metadata(new_doc, "title") <- sprintf("Card %d Title", i)
     new_doc
   })
 
-  reporter <- teal.reporter::Reporter$new()
+  reporter <- Reporter$new()
   reporter$append_cards(cards)
   reporter
 }
@@ -16,14 +16,14 @@ start_reporter_preview_app <- function(name) {
 
   reporter <- create_test_reporter(2)
 
-  # Prefix is necessary to avoid warning "'package:teal.reporter' may not be available when loading"
+  # suppressWarnings is necessary to avoid warning "'package:teal.reporter' may not be available when loading"
   testapp <- shiny::shinyApp(
     ui = shiny::fluidPage(
       shinyjs::useShinyjs(),
-      teal.reporter::reporter_previewer_ui("preview")
+      reporter_previewer_ui("preview")
     ),
     server = function(input, output, session) {
-      teal.reporter::reporter_previewer_srv(
+      reporter_previewer_srv(
         "preview",
         reporter = reporter,
         rmd_output = c("html" = "html_document"),
@@ -39,24 +39,23 @@ start_reporter_preview_app <- function(name) {
   )
 
   app <- tryCatch(
-    shinytest2::AppDriver$new(
-      testapp,
-      name = name,
-      options = list(
-        chromePath = NULL,
-        windowSize = c(1000, 800),
-        browserOptions = list(position = NULL, debug = FALSE)
-      ),
-      seed = 123,
-      timeout = default_idle_timeout
+    suppressWarnings(
+      shinytest2::AppDriver$new(
+        testapp,
+        name = name,
+        options = list(
+          chromePath = NULL,
+          windowSize = c(1000, 800),
+          browserOptions = list(position = NULL, debug = FALSE)
+        ),
+        seed = 123,
+        timeout = default_idle_timeout
+      )
     ),
     error = function(e) skip(paste("Could not initialize AppDriver:", e$message))
   )
 
-  withr::defer(
-    try(app$stop(), silent = TRUE),
-    envir = parent.frame()
-  )
+  withr::defer_parent(try(app$stop(), silent = TRUE))
 
   app$wait_for_idle()
   app
