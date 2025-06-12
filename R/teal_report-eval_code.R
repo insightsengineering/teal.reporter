95cbdd3cfb9925e2e130c9ec78fc80fb41648555
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-06-12 01:09:40 +0100

    feat: move keep_output to eval_code.teal_report

--- R/teal_report-eval_code.R
+++ R/teal_report-eval_code.R
@@ -2,20 +2,39 @@
 setMethod(
   "eval_code",
   signature = c(object = "teal_report"),
-  function(object, code, keep_output = FALSE, code_block_opts = list(), ...) {
-    new_object <- methods::callNextMethod(object = object, code = code, keep_output = keep_output, ...)
+  function(object, code, keep_output = NULL, code_block_opts = list(), ...) {
+    new_object <- methods::callNextMethod(object = object, code = code, ...)
     if (inherits(new_object, "error")) {
       return(new_object)
     }
+
+    if (isTRUE(keep_output)) {
+      keep_output <- setdiff(
+        ls(new_object, all.names = TRUE, sorted = TRUE), ls(object, all.names = TRUE)
+      )
+    } else if (isFALSE(keep_output)) {
+      keep_output <- NULL
+    }
+
+    checkmate::assert(
+      combine = "and",
+      .var.name = "keep_output",
+      checkmate::check_character(keep_output, null.ok = TRUE),
+      checkmate::check_subset(keep_output, ls(new_object, all.names = TRUE), empty.ok = TRUE)
+    )
     temporary_q <- teal.code::qenv()
     temporary_q@code <- setdiff(new_object@code, object@code)
     new_code <- teal.code::get_code(temporary_q)
     if (length(new_code)) {
       teal_card(new_object) <- c(
-        teal_card(object),
-        do.call(code_chunk, args = c(list(code = new_code), code_block_opts)), # TODO: cache an attribute of code chunk
-        attr(new_object@code[[length(new_object@code)]], "output")
+        teal_card(new_object),
+        do.call(code_chunk, args = c(list(code = new_code), code_block_opts))
       )
+      teal_card(new_object) <- Reduce(
+        function(result, this) c(result, new_object[[this]]),
+        init = teal_card(new_object),
+        x = keep_output
+      ) # TODO: cache an attribute of code chunk
     }
     new_object
   }
