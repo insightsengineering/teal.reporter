b345497c8071e1b53312ded1d1a84167130b838b
m7pr <marcin.kosinski.mk1@roche.com>
2025-06-16 13:28:34 +0200

    test for c.teal_report method

---
+++ tests/testthat/test-teal_report-c.R
@@ -0,0 +1,10 @@
+testthat::test_that("c.teal_report concatenates multiple teal_report objects and concatenates teal_card objects", {
+  treport1 <- teal_report(code = "x = 1", teal_card = teal_card("Text 1"))
+  treport2 <- teal_report(code = "x = 2", teal_card = teal_card("Text 2"))
+  treport3 <- teal_report(code = "x = 3", teal_card = teal_card("Text 3"))
+
+  combined_treport <- c(treport1, treport2, treport3)
+
+  testthat::expect_length(teal_card(combined_treport), 3)
+  testthat::expect_equal(teal_card(combined_treport), c(teal_card(treport1), teal_card(treport2), teal_card(treport3))) 
+})
