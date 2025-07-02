fecafde19c63e38dba512493a61f9a293491b6fa
Dawid Kaledkowski <dawid.kaledkowski@gmail.com>
2025-07-02 08:25:22 +0200

    Nice to support Report as well

--- R/render.r
+++ R/render.r
@@ -31,7 +31,7 @@ render <- function(
     global_knitr = getOption("teal.reporter.global_knitr"),
     keep_rmd = TRUE,
     ...) {
-  checkmate::assert_multi_class(input, c("teal_report", "teal_card"))
+  checkmate::assert_multi_class(input, c("teal_report", "teal_card", "Reporter"))
   checkmate::assert_string(output_dir)
   checkmate::assert_list(rmd_yaml_args, names = "named")
   checkmate::assert_list(global_knitr, names = "named")
