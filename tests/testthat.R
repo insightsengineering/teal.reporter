940c80fb3537e9e395b438046f683251626aeb6c
Marcin <133694481+m7pr@users.noreply.github.com>
2025-09-23 14:10:12 +0200

    predefine reporter
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,7 @@
 pkg_name <- "teal.reporter"
 library(pkg_name, character.only = TRUE)
-testthat::test_check(pkg_name)
+reporter <- testthat::MultiReporter$new(list(
+  testthat::CheckReporter$new(),
+  testthat::JunitReporter$new(file = "junit-result.xml")
+))
+testthat::test_check(pkg_name, reporter = reporter)
