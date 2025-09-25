fa2c9181c7631be6ba8b6807233293e094b0820f
Marcin <133694481+m7pr@users.noreply.github.com>
2025-09-25 11:58:43 +0200

    Update tests/testthat.R
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,2 @@
 pkg_name <- "teal.reporter"
 library(pkg_name, character.only = TRUE)
-reporter <- testthat::MultiReporter$new(list(
-  testthat::CheckReporter$new(),
-  testthat::JunitReporter$new(file = "junit-result.xml")
-))
-testthat::test_check(pkg_name, reporter = reporter)
