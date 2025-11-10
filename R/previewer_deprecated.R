b6f14575ebffa5d69c160a2faad9823159d5c943
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-11-10 11:00:54 +0000

    fix: readd RcodeBlock to prevent issue with teal

--- R/previewer_deprecated.R
+++ R/previewer_deprecated.R
@@ -127,3 +127,206 @@ reporter_previewer_srv <- function(id,
     reporter_previewer_content_srv("previewer", reporter = reporter)
   })
 }
+
+#' @title `ContentBlock`: A building block for report content
+#' @docType class
+#' @description This class represents a basic content unit in a report,
+#' such as text, images, or other multimedia elements.
+#' It serves as a foundation for constructing complex report structures.
+#'
+#' @keywords internal
+ContentBlock <- R6::R6Class( # nolint: object_name_linter.
+  classname = "ContentBlock",
+  public = list(
+    #' @description Sets content of this `ContentBlock`.
+    #'
+    #' @param content (`any`) R object
+    #'
+    #' @return `self`, invisibly.
+    #' @examples
+    #' ContentBlock <- getFromNamespace("ContentBlock", "teal.reporter")
+    #' block <- ContentBlock$new()
+    #' block$set_content("Base64 encoded picture")
+    #'
+    set_content = function(content) {
+      private$content <- content
+      invisible(self)
+    },
+    #' @description Retrieves the content assigned to this block.
+    #'
+    #' @return object stored in a `private$content` field
+    #' @examples
+    #' ContentBlock <- getFromNamespace("ContentBlock", "teal.reporter")
+    #' block <- ContentBlock$new()
+    #' block$get_content()
+    #'
+    get_content = function() {
+      private$content
+    },
+    #' @description Create the `ContentBlock` from a list.
+    #'
+    #' @param x (`named list`) with two fields `text` and `style`.
+    #' Use the `get_available_styles` method to get all possible styles.
+    #'
+    #' @return `self`, invisibly.
+    from_list = function(x) {
+      invisible(self)
+    },
+    #' @description Convert the `ContentBlock` to a list.
+    #'
+    #' @return `named list` with a text and style.
+    to_list = function() {
+      list()
+    }
+  ),
+  private = list(
+    content = NULL, # this can be any R object
+    # @description The copy constructor.
+    #
+    # @param name (`character(1)`) the name of the field
+    # @param value the value assigned to the field
+    #
+    # @return the value of the copied field
+    deep_clone = function(name, value) {
+      if (name == "content" && checkmate::test_file_exists(value)) {
+        extension <- ""
+        split <- strsplit(basename(value), split = "\\.")
+        # The below ensures no extension is found for files such as this: .gitignore but is found for files like
+        # .gitignore.txt
+        if (length(split[[1]]) > 1 && split[[1]][length(split[[1]]) - 1] != "") {
+          extension <- split[[1]][length(split[[1]])]
+          extension <- paste0(".", extension)
+        }
+        copied_file <- tempfile(fileext = extension)
+        file.copy(value, copied_file, copy.date = TRUE, copy.mode = TRUE)
+        copied_file
+      } else {
+        value
+      }
+    }
+  ),
+  lock_objects = TRUE,
+  lock_class = TRUE
+)
+
+
+#' @title `RcodeBlock`
+#' @docType class
+#' @description
+#' Specialized `ContentBlock` designed to embed `R` code in reports.
+#'
+#' @keywords internal
+RcodeBlock <- R6::R6Class( # nolint: object_name_linter.
+  classname = "RcodeBlock",
+  inherit = ContentBlock,
+  public = list(
+    #' @description Initialize a `RcodeBlock` object.
+    #'
+    #' @details Returns a `RcodeBlock` object with no content and no parameters.
+    #'
+    #' @param content (`character(1)` or `character(0)`) a string assigned to this `RcodeBlock`
+    #' @param ... any `rmarkdown` `R` chunk parameter and it value.
+    #'
+    #' @return Object of class `RcodeBlock`, invisibly.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #'
+    initialize = function(content = character(0), ...) {
+      checkmate::assert_class(content, "character")
+      super$set_content(content)
+      self$set_params(list(...))
+      invisible(self)
+    },
+    #' @description Sets content of this `RcodeBlock`.
+    #'
+    #' @param content (`any`) R object
+    #'
+    #' @return `self`, invisibly.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$set_content("a <- 1")
+    #'
+    set_content = function(content) {
+      checkmate::assert_string(content)
+      super$set_content(content)
+    },
+    #' @description Sets the parameters of this `RcodeBlock`.
+    #'
+    #' @details Configures `rmarkdown` chunk parameters for the `R` code block,
+    #' influencing its rendering and execution behavior.
+    #'
+    #' @param params (`list`) any `rmarkdown` R chunk parameter and its value.
+    #'
+    #' @return `self`, invisibly.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$set_params(list(echo = TRUE))
+    #'
+    set_params = function(params) {
+      checkmate::assert_list(params, names = "named")
+      checkmate::assert_subset(names(params), self$get_available_params())
+      private$params <- params
+      invisible(self)
+    },
+    #' @description Get the parameters of this `RcodeBlock`.
+    #'
+    #' @return `character` the parameters of this `RcodeBlock`.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$get_params()
+    #'
+    get_params = function() {
+      private$params
+    },
+    #' @description Get available array of parameters available to this `RcodeBlock`.
+    #'
+    #' @return A `character` array of parameters.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$get_available_params()
+    #'
+    get_available_params = function() {
+      names(knitr::opts_chunk$get())
+    },
+    #' @description Create the `RcodeBlock` from a list.
+    #'
+    #' @param x (`named list`) with two fields `text` and `params`.
+    #' Use the `get_available_params` method to get all possible parameters.
+    #'
+    #' @return `self`, invisibly.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$from_list(list(text = "sth", params = list()))
+    #'
+    from_list = function(x) {
+      checkmate::assert_list(x)
+      checkmate::assert_names(names(x), must.include = c("text", "params"))
+      self$set_content(x$text)
+      self$set_params(x$params)
+      invisible(self)
+    },
+    #' @description Convert the `RcodeBlock` to a list.
+    #'
+    #' @return `named list` with a text and `params`.
+    #' @examples
+    #' RcodeBlock <- getFromNamespace("RcodeBlock", "teal.reporter")
+    #' block <- RcodeBlock$new()
+    #' block$to_list()
+    #'
+    to_list = function() {
+      list(text = self$get_content(), params = self$get_params())
+    }
+  ),
+  private = list(
+    content = character(0),
+    params = list()
+  ),
+  lock_objects = TRUE,
+  lock_class = TRUE
+)
